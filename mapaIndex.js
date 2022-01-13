6ac304451b53e292df36444672e5871e9c47dc4a
JesusAlejandroMR <alejandroje14@hotmail.com>
2022-01-13 00:43:27 -0500

    actualización de js de carga de páginas

--- mapaIndex.js
+++ mapaIndex.js
@@ -1,9 +1,9 @@
 function elegirPlataforma(dato){
-    //alert(dato);
+    alert(dato + "actualizando conocimientos de git");
     console.log(dato);
     if(dato.opcion == 1){
-        window.location.href = 'pizarra_Teclado.html';
+        window.location.href = 'pizarra_Teclado.html/'+dato.detalle;
     }else if(dato.opcion == 2){
-        window.location.href = 'pizarra_Mouse.html'
+        window.location.href = 'pizarra_Mouse.html'+dato.detalle;
     }
 }
